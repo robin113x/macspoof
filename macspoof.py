442f41d330d6257f751bba4c4b94afc206a84d7a
Robin <58402504+robin113x@users.noreply.github.com>
2019-12-30 11:29:35 -0500

    Add files via upload

---
+++ macspoof.py
@@ -0,0 +1,22 @@
+#!/usr/bin/evn python
+import pyfiglet
+import subprocess
+import optparse
+parse = optparse.OptionParser()
+b_b = pyfiglet.figlet_format("\nMAC  CHANGER")
+print(b_b)
+print("\t\t\t\t\t\t\t\t-Robin\n")
+face = raw_input("\nEnter the Network Interface(wlan0/eth0) = ")
+if not face:
+    parse.error("[-] please specify your interface i.e wlan0 or eth0")
+mc = raw_input("\nEnter new MAC Address (Ex xx:xx:xx:xx:xx:xx) = ")
+if not mc:
+    parse.error("[-] please Enter New MAC ADDRESS")
+print("\n************************************************************")
+print("MAC Address of network interface " + face + "change to " + mc)
+print("************************************************************\n")
+subprocess.call(["ifconfig", face, "down"])
+subprocess.call(["ifconfig", face, "ether", "hw", mc])
+subprocess.call(["ifconfig", face, "up"])
+subprocess.call(["ifconfig", face])
+
